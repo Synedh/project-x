416ec0e19a2dc167da96ec8f5c4d18b866a267b1
Synedh <synedh.off@gmail.com>
2018-07-03 16:19:26 +0200

    CurrentEntityBox

---
+++ Assets/Scripts/Entity/CurrentEntityBoxBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrentEntityBoxBehaviour : MonoBehaviour {
+
+    Character currentCharacter;
+
+    Text currentHP;
+    Text currentAP;
+    Text currentMP;
+
+	// Use this for initialization
+    void Start () {
+        currentHP = transform.Find("HP/Text").GetComponent<Text>();
+        currentAP = transform.Find("AP/Text").GetComponent<Text>();
+        currentMP = transform.Find("MP/Text").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentCharacter = GameManager.instance.selectedEntity.GetComponent<EntityBehaviour>().character;
+        currentHP.text = currentCharacter.stats[Characteristic.CurrentHP].ToString();
+        currentAP.text = currentCharacter.stats[Characteristic.CurrentAP].ToString();
+        currentMP.text = currentCharacter.stats[Characteristic.CurrentMP].ToString();
+	}
+}
